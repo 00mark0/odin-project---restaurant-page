e692d131419c69358f87a997496244d4bbb133c9
00mark0 <00marko.r@gmail.com>
2024-04-04 00:34:46 +0200

    removed labels and added aria-labels for inputs in the booking form

--- src/pages/booking/components/reservations.js
+++ src/pages/booking/components/reservations.js
@@ -31,8 +31,7 @@ const reservationsContent = `
         <h3>Reservation</h3>
         <p>BOOK YOUR TABLE</p>
         <form id="reservationForm">
-          <label for="people"></label>
-            <select id="people" name="people" autocomplete="on">
+            <select id="people" name="people" autocomplete="on" aria-label="People">
                 <option value="1">1 person</option>
                 <option value="2" selected>2 people</option>
                 <option value="3">3 people</option>
@@ -44,16 +43,11 @@ const reservationsContent = `
                 <option value="9">9 people</option>
                 <option value="10">10 people</option> 
             </select>
-            <label for="date"></label>
-            <input type="date" id="date" name="date" placeholder="Date" required autocomplete="on">
-            <label for="time"></label>
-            <input type="time" id="time" name="time" min="10:00" max="2:00" placeholder="Time" required autocomplete="on">
-            <label for="name"></label>
-            <input type="text" id="name" name="name" placeholder="Name" required autocomplete="on">
-            <label for="phone"></label>
-            <input type="tel" id="phone" name="phone" placeholder="Phone" required autocomplete="on">
-            <label for="email"></label>
-            <input type="email" id="email" name="email" placeholder="Email" required autocomplete="on">
+            <input type="date" id="date" name="date" placeholder="Date" required autocomplete="on" aria-label="Date">
+            <input type="time" id="time" name="time" min="10:00" max="2:00" placeholder="Time" required autocomplete="on" aria-label="Time">
+            <input type="text" id="name" name="name" placeholder="Name" required autocomplete="on" aria-label="Name">
+            <input type="tel" id="phone" name="phone" placeholder="Phone" required autocomplete="on" aria-label="Phone">
+            <input type="email" id="email" name="email" placeholder="Email" required autocomplete="on" aria-label="Email">
         </form>
         <button type="submit" form="reservationForm">BOOK NOW</button>
     </div>
